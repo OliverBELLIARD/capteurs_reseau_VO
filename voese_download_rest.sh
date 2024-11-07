f5c0500a61b47d8ebe63430027936413a4f67e06
oliver2ba <102949323+OliverBELLIARD@users.noreply.github.com>
2024-11-07 21:52:09 +0100

    Clarification of voese_download_rest.sh

--- voese_download_rest.sh
+++ voese_download_rest.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/expect -f
+
+# This script allows to download the contents in the folder where we stored our REST Server in the Raspberry Pi
+# It has to be made an executable with: chmod +x voese_download_rest.sh
+# Warning: you need the expect command: sudo apt  install expect
+
 set timeout -1
 spawn sftp vo@192.168.88.230
 expect "password:"
@@ -7,6 +12,3 @@ expect "sftp>"
 send -- "get -r REST_server/\n"
 expect "sftp>"
 send -- "bye\n"
-
-# It has to be made an executable with: chmod +x voese_download_rest.sh
-# Warning: you need the expect command: sudo apt  install expect
