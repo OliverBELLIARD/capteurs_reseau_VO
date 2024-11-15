65ee78a90529e519e5e5477507031c350ec9429c
valerian <valerian.priou@ensea.fr>
2024-11-15 17:55:10 +0100

    FLASK GET_T GET_P

--- REST_server/hello.py
+++ REST_server/hello.py
@@ -1,26 +1,83 @@
 import json
-from flask import jsonify
 from flask import Flask
+from flask import jsonify
 from flask import render_template
 from flask import abort
 from flask import request
+import serial
 app = Flask(__name__)
+ser = serial.serial_for_url(f'socket://192.168.88.235:5000', timeout=1)
+
 
 @app.route('/')
 def hello_world():
-	return jsonify({"message": 'Hello, World!'})
+    return jsonify({"message": 'Hello, World!'})
 
 welcome = "Welcome to 3ESE API!"
 
 @app.route('/api/welcome/')
 def api_welcome():
-    return jsonify({"message": welcome})
-    
-@app.route('/api/welcome/<int:index>', methods=['GET','POST'])
+    if request.method == 'GET' :
+        return jsonify({"phrase" : welcome})
+
+@app.route('/api/welcome/<int:index>', methods=['GET','POST','DELETE'])
 def api_welcome_index(index):
     if index < 0 or index >= len(welcome):
         abort(404)
-    return jsonify({"index": index, "val": welcome[index]})
+
+    if request.method == 'GET' :
+        return jsonify({"index" : index, "value" : welcome[index]})
+
+    #elif request.method == 'POST' :
+
+    #return jsonify({"index": index, "val": welcome[index]})
+
+def get_temp() :
+    try:
+        ser.write(b'GET_T\n')  # Envoie la commande série "GET_T" pour obtenir la température
+        temp = ser.readline().decode().strip()  # Lit et décode la réponse
+        return temp
+    except serial.SerialException as e:
+        print(f"Erreur de connexion série pour la température : {e}")
+        return None
+
+
+def get_pres() :
+    try:
+        ser.write(b'GET_P\n')  # Envoie la commande série "GET_P" pour obtenir la pression
+        pres = ser.readline().decode().strip()  # Lit et décode la réponse
+        return pres
+    except serial.SerialException as e:
+        print(f"Erreur de connexion série pour la pression : {e}")
+        return None
+
+
+@app.route('/api/<path>', methods=['GET','POST','DELETE'])
+def api_data(path) :
+    if path == 'temp' and request.method == 'GET' :
+        temp = get_temp()
+        if temp is not None :
+            return jsonify({"temperature" : temp})
+        else :
+            return jsonify({"error" : "get_temp() ne fonctionne pas."})
+
+    elif path == 'pres' and request.method == 'GET' :
+        pres = get_pres()
+        if pres is not None :
+            return jsonify({"pressure" : pres})
+        else :
+            return jsonify({"error" : "get_pres() ne fonctionne pas."})
+
+    else :
+        return jsonify({"error" : "path inconnu."})
+
+
+
+
+
+
+
+
 
 @app.errorhandler(404)
 def page_not_found(error):
